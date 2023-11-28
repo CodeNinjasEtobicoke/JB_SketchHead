a0b270d14b5c6b7dba5287f74e4e39e34f4c9219
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2023-11-28 17:27:31 -0500

    what
    idk

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+  
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject.Find("DoodleHead").SetActive(false);
+        GameObject.Find("GameController").GetComponent<GameController>().GameOver();
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    [Header("Game Over UI Canvas Object")]
+    public GameObject gameOverCanvas;
+    [Header("Platform Object")]
+    public GameObject platform;
+
+    float pos = 0;
+
+    void Start()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            SpawnPlatforms();
+        }
+
+    }
+
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void SpawnPlatforms()
+    {
+        Instantiate(platform, new Vector3(Random.value * 10 - 5f, pos, 0.5f), Quaternion.identity);
+        pos += 2.5f;
+    }
+}
